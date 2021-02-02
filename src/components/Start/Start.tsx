f8cc616f3924b7dc507e5715e25af11635267ae2
Ben Graves <btg628@cs.bham.ac.uk>
2021-02-02 22:34:03 +0000

    and then

--- src/components/Start/Start.tsx
+++ src/components/Start/Start.tsx
@@ -31,7 +31,7 @@ export default class Gallery extends React.Component<Props, State> {
         <img src={CompositionII} className="start__painting" />
         <div className="start__title">mondrian.fun</div>
         <div className="start__description">
-          This site is a celebration of the artistic style of Piet Mondrian, a 20th century Dutch artist best known for his abstract works using vertical and horizontal lines and the primary colors. Here you can create your own masterpiece in the style of Mondrian and hang it in the virtual gallery alongside other people's work. Head to the studio to get started or visit the gallery for some inspiration... 
+          This site is a celebration of the artistic style of Piet Mondrian, a 20th century Dutch artist best known for his abstract works using vertical and horizontal lines and the primary colors. Here you can create your own masterpiece in the style of Mondrian and then hang it in the virtual gallery alongside other people's work. Head to the studio to get started or visit the gallery for some inspiration... 
         </div>
         <div onClick={() => this.props.setTab(0, false)} className="gallery__leave">
           {"<- Go to studio"}
